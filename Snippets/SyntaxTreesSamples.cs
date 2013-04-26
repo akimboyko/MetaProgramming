f9f54f531795b4ad75ef0436e05eeb8f81738f47
Akim Boyko <akim.boyko@gmail.com>
2013-04-26 07:24:42 +0300

    Roslyn SyntaxTree sample

---
+++ Snippets/SyntaxTreesSamples.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+public class Examples
+{
+    public void Function()
+    {
+        var x = 6;
+        System.Math.Sqrt(x * 7);
+    }
+
+    public IEnumerable<string> Cases()
+    {
+        yield return "Foo"; 
+    }
+}
