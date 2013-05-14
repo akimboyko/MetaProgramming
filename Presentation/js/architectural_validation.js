c9834ad6eb9f262311c0308d25a6c2ad8e68f8d5
Akim Boyko <akim.boyko@gmail.com>
2013-05-14 07:33:01 +0300

    First version of .Net build pipeline

--- Presentation/js/architectural_validation.js
+++ Presentation/js/architectural_validation.js
@@ -1,41 +1,129 @@
 ;(function() {
-	// helper method to generate a color from a cycle of colors.
-	var curColourIndex = 1, maxColourIndex = 24, nextColour = function() {
-		var R,G,B;
-		R = parseInt(128+Math.sin((curColourIndex*3+0)*1.3)*128);
-		G = parseInt(128+Math.sin((curColourIndex*3+1)*1.3)*128);
-		B = parseInt(128+Math.sin((curColourIndex*3+2)*1.3)*128);
-		curColourIndex = curColourIndex + 1;
-		if (curColourIndex > maxColourIndex) curColourIndex = 1;
-		return "rgb(" + R + "," + G + "," + B + ")";
-	 };	
-
 	jsPlumb.ready(function() {
 		jsPlumb.importDefaults({
-			DragOptions : { cursor: "pointer", zIndex:2000 },
-			HoverClass:"connector-hover"
-		});
+			// default drag options
+			DragOptions : { cursor: 'pointer', zIndex:2000 },
+			// default to blue at one end and green at the other
+			EndpointStyles : [{ fillStyle:'#ffffff' }, { fillStyle:'#000000' }],
+			// blue endpoints 7 px; green endpoints 11.
+			Endpoints : [ [ "Dot", {radius: 3} ], [ "Dot", { radius: 3 } ]],
+			// the overlays to decorate each connection with.  note that the label overlay uses a function to generate the label text; in this
+			// case it returns the 'labelText' member that we set on each connection in the 'init' method below.
+			ConnectionOverlays : [
+				[ "Arrow", { location:1.0 } ],
+				[ "Label", { 
+					location:0.1,
+					id:"label",
+					cssClass:"aLabel"
+				}]
+			]
+		});			
 
-		// initialise draggable elements.  
-		jsPlumb.draggable($(".window"));
+		// this is the paint style for the connecting lines..
+		var connectorPaintStyle = {
+			lineWidth: 4,
+			strokeStyle: "#deea18",
+			joinstyle: "round",
+			outlineColor: "#EAEDEF",
+			outlineWidth: 7
+		},
 
-		var stateMachineConnector = {				
-				connector:"StateMachine",
-				paintStyle:{lineWidth:3,strokeStyle:"#056"},
-				hoverPaintStyle:{strokeStyle:"#dbe300"},
-				endpoint:"Blank",
-				anchor:"Continuous",
-				overlays:[ ["PlainArrow", {location:1, width:15, length:12} ]]
+		// .. and this is the hover style. 
+		connectorHoverStyle = {
+			lineWidth:4,
+			strokeStyle:"#2e2aF8"
+		},
+
+		endpointHoverStyle = {fillStyle:"#2e2aF8"},
+
+		// the definition of source endpoints (the small blue ones)
+		sourceEndpoint = {
+			endpoint:"Rectangle",
+			paintStyle:{ fillStyle:"#225588", radius: 4 },
+			isSource: true,
+			connector:[ "Flowchart", { stub:[40, 60], gap:10, cornerRadius:5 } ],								                
+			connectorStyle:connectorPaintStyle,
+			hoverPaintStyle:endpointHoverStyle,
+			connectorHoverStyle:connectorHoverStyle,
+            dragOptions:{},
+            overlays:[
+            	[ "Label", { 
+                	location:[0.5, 1.5], 
+                	label:" ",
+                	cssClass:"endpointSourceLabel" 
+                } ]
+            ]
+		},
+
+		// a source endpoint that sits at BottomCenter
+		// the definition of target endpoints (will appear when the user drags a connection) 
+		targetEndpoint = {
+			endpoint:"Rectangle",					
+			paintStyle:{ fillStyle:"#558822",radius:4 },
+			hoverPaintStyle:endpointHoverStyle,
+			maxConnections:-1,
+			dropOptions:{ hoverClass:"hover", activeClass:"active" },
+			isTarget:true,			
+            overlays:[
+            	[ "Label", { location:[0.5, -0.5], label:" ", cssClass:"endpointTargetLabel" } ]
+            ]
+		},	
+
+		init = function(connection) {
+			connection.getOverlay("label")/*.setLabel(connection.sourceId.substring(6) + "-" + connection.targetId.substring(6))*/;
+			connection.bind("editCompleted", function(o) {
+				if (typeof console != "undefined")
+					console.log("connection edited. path is now ", o.path);
+			});
+		};			
+
+		var allSourceEndpoints = [], allTargetEndpoints = [];
+			_addEndpoints = function(toId, sourceAnchors, targetAnchors) {
+				for (var i = 0; i < sourceAnchors.length; i++) {
+					var sourceUUID = toId + sourceAnchors[i];
+					allSourceEndpoints.push(jsPlumb.addEndpoint(toId, sourceEndpoint, { anchor:sourceAnchors[i], uuid:sourceUUID }));						
+				}
+				for (var j = 0; j < targetAnchors.length; j++) {
+					var targetUUID = toId + targetAnchors[j];
+					allTargetEndpoints.push(jsPlumb.addEndpoint(toId, targetEndpoint, { anchor:targetAnchors[j], uuid:targetUUID }));						
+				}
 			};
-			
-		jsPlumb.connect({
-			source:"code",
-			target:"cil"
-		}, stateMachineConnector);
+
+		_addEndpoints("code", ["RightMiddle"], [ ]);			
+		_addEndpoints("cil", ["RightMiddle"], ["LeftMiddle"]);
+		_addEndpoints("asm", [], ["LeftMiddle"]);
+		_addEndpoints("modified", ["RightMiddle"], ["LeftMiddle"]);
+					
+		// listen for new connections; initialise them the same way we initialise the connections at startup.
+		jsPlumb.bind("jsPlumbConnection", function(connInfo, originalEvent) { 
+			init(connInfo.connection);
+		});			
+					
+		// make all the window divs draggable						
+		jsPlumb.draggable(jsPlumb.getSelector(".window"), { grid: [20, 20] });
+		// THIS DEMO ONLY USES getSelector FOR CONVENIENCE. Use your library's appropriate selector method!
+		//jsPlumb.draggable(jsPlumb.getSelector(".window"));
+
+        
+		// connect a few up
+		jsPlumb.connect({uuids:["codeRightMiddle", "cilLeftMiddle"], editable:false});
+		jsPlumb.connect({uuids:["cilRightMiddle", "asmLeftMiddle"], editable:true});
+		//
+        
+		//
+		// listen for clicks on connections, and offer to delete connections on click.
+		//
+		jsPlumb.bind("click", function(conn, originalEvent) {
+			if (confirm("Delete connection from " + conn.sourceId + " to " + conn.targetId + "?"))
+				jsPlumb.detach(conn); 
+		});	
 		
-		jsPlumb.connect({
-			source:"cil",
-			target:"asm"
-		}, stateMachineConnector);  
+		jsPlumb.bind("connectionDrag", function(connection) {
+			console.log("connection " + connection.id + " is being dragged");
+		});		
+		
+		jsPlumb.bind("connectionDragStop", function(connection) {
+			console.log("connection " + connection.id + " was dragged");
+		});
 	});
 })();
