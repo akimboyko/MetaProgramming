f24befa0a99b46cf427b8e80810300bdfb3fbf7b
Akim Boyko <akim.boyko@gmail.com>
2013-05-20 07:44:13 +0300

    Remove PostSharp samples for now

--- MetaProgramming/MetaProgramming.RoslynCTP.Tests/CodeSmells.Samples/CodeSmells.FakeDataAccessLibrary/Repository.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace CodeSmells.FakeDataAccessLibrary
-{
-    public class Repository
-    {
-        public static readonly Type NHibernateSessionType = typeof(NHibernate.ISession);
-    }
-}
