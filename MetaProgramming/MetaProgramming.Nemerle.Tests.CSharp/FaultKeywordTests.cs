9627de09357205939c3e734f07ad639ae85eada1
Akim Boyko <akim.boyko@gmail.com>
2013-05-31 06:30:46 +0300

    Test for Nemerle `fault` keyword

---
+++ MetaProgramming/MetaProgramming.Nemerle.Tests.CSharp/FaultKeywordTests.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace MetaProgramming.Nemerle.Tests.CSharp
+{
+    [TestFixture]
+    public class FaultKeywordTests
+    {
+        [Test]
+        public void FaultKeywordAtNemerle_NormalFlow_BodyHasBeenExecuted()
+        {
+            new FaultKeywordSample()
+                    .ExecuteFaultCSharpNemerle(
+                        () => "body",
+                        () => { throw new Exception("Should not execute `fault`"); })
+                    .Should().Be("body");
+        }
+
+        [Test]
+        public void FaultKeywordAtNemerle_ExceptionalFlow_BothBodyAndFaultHadBeenExecuted()
+        {
+            var bodyHasBeenExecuted = false;
+            var faultHasBeenExecuted = false;
+
+            Action act = () =>
+                new FaultKeywordSample()
+                    .ExecuteFaultCSharpNemerle(
+                        () =>
+                        {
+                            bodyHasBeenExecuted = true;
+                            throw new Exception("Exceptional Flow");
+                        },
+                        () =>
+                        {
+                            faultHasBeenExecuted = true;
+                        });
+
+            act.ShouldThrow<Exception>()
+                .WithMessage("Exceptional Flow");
+
+            bodyHasBeenExecuted.Should().BeTrue();
+            faultHasBeenExecuted.Should().BeTrue();
+        }
+    }
+}
