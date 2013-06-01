11424620b2d75fb14f8dd4da72d7bcec99d03ec1
Akim Boyko <akim.boyko@gmail.com>
2013-06-01 07:37:52 +0300

    Add schema for Roslyn + T4; add Nemerle macro description

---
+++ Presentation/js/metaprogramming_roslyn.js
@@ -0,0 +1,143 @@
+(function() {
+    "use strict";
+    jsPlumb.ready(function() {
+        jsPlumb.importDefaults({
+            // default drag options
+            DragOptions : { cursor: 'pointer', zIndex: 2000 },
+            // default to blue at one end and green at the other
+            EndpointStyles : [{ fillStyle: '#ffffff' }, { fillStyle: '#000000' }],
+            // blue endpoints 7 px; green endpoints 11.
+            Endpoints : [ [ "Dot", {radius: 3} ], [ "Dot", { radius: 3 } ]],
+            // the overlays to decorate each connection with.  note that the label overlay uses a function to generate the label text; in this
+            // case it returns the 'labelText' member that we set on each connection in the 'init' method below.
+            ConnectionOverlays : [
+                [ "Arrow", { location: 1.0 } ],
+                [ "Label", { 
+                    location: 0.1,
+                    id: "label",
+                    cssClass: "aLabel"
+                } ]
+            ]
+        });            
+
+        // this is the paint style for the connecting lines..
+        var connectorPaintStyle = {
+            lineWidth: 4,
+            strokeStyle: "#deea18",
+            joinstyle: "round",
+            outlineColor: "#EAEDEF",
+            outlineWidth: 7
+        },
+
+        // .. and this is the hover style. 
+        connectorHoverStyle = {
+            lineWidth: 4,
+            strokeStyle: "#2e2aF8"
+        },
+
+        endpointHoverStyle = {fillStyle: "#2e2aF8"},
+
+        // the definition of source endpoints (the small blue ones)
+        sourceEndpoint = {
+            endpoint: "Rectangle",
+            paintStyle: { fillStyle: "#225588", radius: 4 },
+            isSource: true,
+            connector: [ "Flowchart", { stub: [40, 60], gap: 10, cornerRadius: 5 } ],                                                
+            connectorStyle: connectorPaintStyle,
+            hoverPaintStyle: endpointHoverStyle,
+            connectorHoverStyle: connectorHoverStyle,
+            dragOptions: {},
+            overlays: [
+                [ "Label", { 
+                    location: [0.5, 1.5], 
+                    label: " ",
+                    cssClass: "endpointSourceLabel" 
+                } ]
+            ]
+        },
+
+        // a source endpoint that sits at BottomCenter
+        // the definition of target endpoints (will appear when the user drags a connection) 
+        targetEndpoint = {
+            endpoint: "Rectangle",                    
+            paintStyle: { fillStyle: "#558822", radius: 4 },
+            hoverPaintStyle: endpointHoverStyle,
+            maxConnections: -1,
+            dropOptions: { hoverClass: "hover", activeClass: "active" },
+            isTarget: true,            
+            overlays: [
+                [ "Label", { location: [ 0.5, -0.5 ], label: " ", cssClass: "endpointTargetLabel" } ]
+            ]
+        },    
+
+        init = function(connection) {
+            connection.getOverlay("label");
+            connection.bind("editCompleted", function(o) {
+                if (typeof console !== "undefined") {
+                    console.log("connection edited. path is now ", o.path);
+                }
+            });
+        },         
+
+        allSourceEndpoints = [],
+
+        addEndpointsFunc,
+
+        allTargetEndpoints = [];
+            addEndpointsFunc = function(toId, sourceAnchors, targetAnchors) {
+                for (var i = 0; i < sourceAnchors.length; i++) {
+                    var sourceUUID = toId + sourceAnchors[i];
+                    allSourceEndpoints.push(jsPlumb.addEndpoint(toId, sourceEndpoint, { anchor:sourceAnchors[i], uuid:sourceUUID }));                        
+                }
+
+                for (var j = 0; j < targetAnchors.length; j++) {
+                    var targetUUID = toId + targetAnchors[j];
+                    allTargetEndpoints.push(jsPlumb.addEndpoint(toId, targetEndpoint, { anchor:targetAnchors[j], uuid:targetUUID }));                        
+                }
+            };
+
+        addEndpointsFunc("ScriptJson", ["BottomCenter"], [ ]);            
+        addEndpointsFunc("DtoJson", ["BottomCenter"], [ ]);            
+        addEndpointsFunc("InputJson", ["BottomCenter"], [ ]);  
+
+        addEndpointsFunc("RoslynCreatesCubmission", ["BottomCenter"], ["TopCenter"]);
+        addEndpointsFunc("RuntimeT4", ["BottomCenter"], ["TopCenter"]);
+        addEndpointsFunc("RoslynDynamicAssembly", ["RightMiddle"], ["TopCenter"]);
+
+
+        addEndpointsFunc("AppDomain", ["RightMiddle"], ["TopCenter", "LeftMiddle"]);
+        addEndpointsFunc("Processing", [], ["TopCenter", "LeftMiddle"]);
+                    
+        // listen for new connections; initialise them the same way we initialise the connections at startup.
+        jsPlumb.bind("jsPlumbConnection", function(connInfo, originalEvent) { 
+            init(connInfo.connection);
+        });            
+                    
+        // make all the window divs draggable                        
+        jsPlumb.draggable(jsPlumb.getSelector(".window"), { grid: [20, 20] });
+        // THIS DEMO ONLY USES getSelector FOR CONVENIENCE. Use your library's appropriate selector method!
+        //jsPlumb.draggable(jsPlumb.getSelector(".window"));
+
+        
+        // connect a few up
+        //jsPlumb.connect({uuids:["codeRightMiddle", "cilLeftMiddle"], editable:false});
+        //jsPlumb.connect({uuids:["cilRightMiddle", "asmLeftMiddle"], editable:true});
+        //
+        
+        //
+        // listen for clicks on connections, and offer to delete connections on click.
+        //
+        jsPlumb.bind("click", function(conn, originalEvent) {
+            if (confirm("Delete connection from " + conn.sourceId + " to " + conn.targetId + "?"))
+                jsPlumb.detach(conn); 
+        });    
+        
+        jsPlumb.bind("connectionDrag", function(connection) {
+            console.log("connection " + connection.id + " is being dragged");
+        });        
+        
+        jsPlumb.bind("connectionDragStop", function(connection) {
+            console.log("connection " + connection.id + " was dragged");
+        });
+    });
+})();
